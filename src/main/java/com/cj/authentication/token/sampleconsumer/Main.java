319db1658ea73dabffd273945818feacd6990ec6
Alexis King <lexi.lambda@gmail.com>
2017-06-09 16:14:01 -0700

    CJPM-4613: Update example to use asymmetric cryptography support

--- src/main/java/com/cj/authentication/token/sampleconsumer/Main.java
+++ src/main/java/com/cj/authentication/token/sampleconsumer/Main.java
@@ -3,20 +3,24 @@ package com.cj.authentication.token.sampleconsumer;
 import com.cj.authentication.Token;
 import com.cj.authentication.TokenVerifier;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Optional;
 
 public class Main {
-  public static void main(String[] args) {
-    if (args.length != 2) {
-      System.out.println("usage: authentication-token-sample-consumer <secret> <token>");
+  public static void main(String[] args) throws MalformedURLException {
+    if (args.length != 1) {
+      System.out.println("usage: authentication-token-sample-consumer <token>");
       System.exit(1);
     }
-    Optional<Token> token = TokenVerifier.verifyTokenString(args[0], args[1]);
-    if (token.isPresent()) {
-      System.out.println(token.get());
-    } else {
-      System.err.println("invalid token");
-      System.exit(1);
+    try (TokenVerifier tokenVerifier = new TokenVerifier(new URL("https://lab-io.d.cjpowered.com/public-keys"))) {
+      Optional<Token> token = tokenVerifier.verifyTokenString(args[0]);
+      if (token.isPresent()) {
+        System.out.println(token.get());
+      } else {
+        System.err.println("invalid token");
+        System.exit(1);
+      }
     }
   }
 }
